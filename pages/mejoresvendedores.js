df44f6d51a69a9048443855016baa01ca76bc6cf
Alejandro Isa <alejandroisa71@gmail.com>
2020-09-30 22:57:56 -0300

    se agregan graficas con Recharts par ver mejores vendedores y mejores clientes

---
+++ pages/mejoresvendedores.js
@@ -0,0 +1,81 @@
+import React, { useEffect } from "react";
+import Layout from "../components/Layout";
+import { gql, useQuery } from "@apollo/client";
+
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
+
+const MEJORES_VENDEDORES = gql`
+  query mejoresVendedores {
+    mejoresVendedores {
+      vendedor {
+        nombre
+        email
+      }
+      total
+    }
+  }
+`;
+
+const MejoresVendedores = () => {
+  const { data, loading, error, startPolling, stopPolling } = useQuery(
+    MEJORES_VENDEDORES
+  );
+
+  useEffect(() => {
+    startPolling(1000);
+    return () => {
+      stopPolling();
+    };
+  }, [startPolling, startPolling]);
+
+  if (loading) return "Cargando";
+
+  //   console.log(data);
+
+  const { mejoresVendedores } = data;
+
+  const vendedorGrafica = [];
+
+  mejoresVendedores.map((vendedor, index) => {
+    vendedorGrafica[index] = {
+      ...vendedor.vendedor[0],
+      total: vendedor.total,
+    };
+  });
+  console.log(vendedorGrafica);
+
+  return (
+    <Layout>
+      <h1>Mejores Vendedores</h1>
+      <BarChart
+        className="mt-10"
+        width={600}
+        height={500}
+        data={vendedorGrafica}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="nombre" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="total" fill="#3182CE" />
+      </BarChart>
+    </Layout>
+  );
+};
+
+export default MejoresVendedores;
