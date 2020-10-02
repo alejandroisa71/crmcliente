639ee6ec0980f9c6e22c1e7e55b42204c9b29df0
Alejandro Isa <alejandroisa71@gmail.com>
2020-10-02 02:36:14 -0300

    configuracion de uri

--- config/apollo.js
+++ config/apollo.js
@@ -3,7 +3,7 @@ import fetch from "node-fetch";
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:4000/",
+  uri: "https://vast-lake-38490.herokuapp.com/",
   fetch,
 });
 
